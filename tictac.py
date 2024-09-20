78b1ffc0d30e80074ab3c964e29753118d9a5fad
Carter Markham <cmarkham7272@gmail.com>
2024-09-19 20:40:42 -0500

    Re-wrote the board creation function. Still not working.

--- tictac.py
+++ tictac.py
@@ -1,6 +1,5 @@
 # TIC TAC TOE CLI APPLICATION
 
-# TODO create function that displays the tic tac toe game title
 # TODO create function that asks for the users move (will need a while loop to continuously run game until winner or "quit")
 # TODO when user inputs his move. CLI responds with overview of the board
 # TODO CLI loops over board and has a RANDOM response
@@ -24,29 +23,33 @@ def start_game():
         
         """)
   
-def print_board_status(board_status):
-    sep = "\n" + "-" * (size * 4 - 3) + "\n"
-    print("\n" + sep.join(" | ".join(col) for col in board_status) + "\n")
-  
-def player_input():
-  while True:
+
+  # This creates a function that separates the sections of the game
+  def print_board_status():
+    board = []
+    noRow = 3
+    noCol = 3
+    for r in range(0, noRow, 1):
+      board.append([])
+      for c in range(0, noCol, 1):
+        board[r].append("*")
+
+      for r in range(0, noRow, 1):
+        for c in range(0, noCol, 1):
+          print(board[r][c], end = "")
+  print()
+
+  def player_input():
+    while True:
       try:
         nrow = print(int(f"In which column would you like to place your move?"))
         ncol = print(int(f"In which column would you like to place your move?"))
       except ValueError:
         print("Invalid Entry")
+        break
   
-  
-  
-
-  # while True:
-  #   print("What is your move?")
-  #   user_choice = input()
-  #   for row in board:
-  #     for column in row:
-  #       print(user_choice, '')
-
-print_board_status()
+  print_board_status()
+  player_input()
 
 
 
